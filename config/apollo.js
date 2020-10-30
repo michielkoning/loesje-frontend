8fea9afa15bf03c6107b54e7e2a32398ef58e56d
Michiel Koning <michiel.koning@valtech.com>
2020-10-30 09:44:15 +0100

    possible types

--- config/apollo.js
+++ config/apollo.js
@@ -1,9 +1,12 @@
+import { InMemoryCache } from '@apollo/client/core'
 import { apiUrl } from './../data/siteDetails'
-
+import possibleTypes from './../data/possibleTypes.json'
+const cache = new InMemoryCache({ possibleTypes })
 export default {
   clientConfigs: {
     default: {
       // required
+      cache,
       httpEndpoint: `${apiUrl}graphql`,
       httpLinkOptions: {
         credentials: 'include',
